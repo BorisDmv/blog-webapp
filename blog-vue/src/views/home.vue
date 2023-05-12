3e2ec24ff1ce474861de6066aad2cabfaefd39b3
Boris <borisdimitrov1998@gmail.com>
2023-05-12 23:26:40 +0300

    reading specific blog through read me button

--- blog-vue/src/views/home.vue
+++ blog-vue/src/views/home.vue
@@ -2,46 +2,52 @@
     <div class="container">
         <textarea v-model="markdown"></textarea>
         <div v-html="markdownToHtml"></div>
-        
+
+
         <div v-for="blog in blogs">
             <div>
                 <p>Title is {{ blog.attributes.title }}</p>
                 <p>Title is {{ blog.attributes.desc }}</p>
-                <button>Read more</button>
+                <button @click="ReadBlog(blog.id)">Read more</button>
             </div>
         </div>
     </div>
 </template>
 <script>
-import { marked } from 'marked';
+import {marked} from 'marked';
 
 export default {
-    data(){
-      return {
-        markdown: "# Hello World",
-        blogs: []
-      };
-  },
-  computed: {
-      markdownToHtml(){
-          return marked(this.markdown);
-      }
-  },
-  created(){
-      this.$axios.get('blogs')
-      .then(response => {
-          console.log(response.data.data);
-          this.blogs = response.data.data
-      })
-      .catch(error => {
-          console.error(error);
-      });
-  }
+    data() {
+        return {
+            markdown: "# Hello World",
+            blogs: []
+        };
+    },
+    methods: {
+        ReadBlog(blogID) {
+            this.$router.push(`/blog/${blogID}`)
+        }
+    },
+    computed: {
+        markdownToHtml() {
+            return marked(this.markdown);
+        }
+    },
+    created() {
+        this.$axios.get('blogs')
+            .then(response => {
+                console.log(response.data.data);
+                this.blogs = response.data.data
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
 }
 </script>
 
 <style scoped>
-.container{
+.container {
     margin: 0 auto;
     margin-top: 136px;
 }
